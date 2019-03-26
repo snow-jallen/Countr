1f8aceb0c9c480e21080e2c19a3bf1380d906b7b
Jonathan Allen <jallen@snow.edu>
2019-03-26 15:07:34 -0600

    add camera functionality

--- Countr.Droid/Resources/Resource.Designer.cs
+++ Countr.Droid/Resources/Resource.Designer.cs
@@ -6497,8 +6497,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090039
 			public const int SYM = 2131296313;
 			
-			// aapt resource value: 0x7f0900a2
-			public const int action0 = 2131296418;
+			// aapt resource value: 0x7f0900a4
+			public const int action0 = 2131296420;
 			
 			// aapt resource value: 0x7f09007e
 			public const int action_bar = 2131296382;
@@ -6521,17 +6521,17 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09005c
 			public const int action_bar_title = 2131296348;
 			
-			// aapt resource value: 0x7f09009f
-			public const int action_container = 2131296415;
+			// aapt resource value: 0x7f0900a1
+			public const int action_container = 2131296417;
 			
 			// aapt resource value: 0x7f09007f
 			public const int action_context_bar = 2131296383;
 			
-			// aapt resource value: 0x7f0900a6
-			public const int action_divider = 2131296422;
+			// aapt resource value: 0x7f0900a8
+			public const int action_divider = 2131296424;
 			
-			// aapt resource value: 0x7f0900a0
-			public const int action_image = 2131296416;
+			// aapt resource value: 0x7f0900a2
+			public const int action_image = 2131296418;
 			
 			// aapt resource value: 0x7f090003
 			public const int action_menu_divider = 2131296259;
@@ -6548,14 +6548,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09005e
 			public const int action_mode_close_button = 2131296350;
 			
-			// aapt resource value: 0x7f0900b5
-			public const int action_save_counter = 2131296437;
+			// aapt resource value: 0x7f0900b7
+			public const int action_save_counter = 2131296439;
 			
-			// aapt resource value: 0x7f0900a1
-			public const int action_text = 2131296417;
+			// aapt resource value: 0x7f0900a3
+			public const int action_text = 2131296419;
 			
-			// aapt resource value: 0x7f0900af
-			public const int actions = 2131296431;
+			// aapt resource value: 0x7f0900b1
+			public const int actions = 2131296433;
 			
 			// aapt resource value: 0x7f09005f
 			public const int activity_chooser_view_content = 2131296351;
@@ -6590,11 +6590,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09003f
 			public const int bottom = 2131296319;
 			
+			// aapt resource value: 0x7f090092
+			public const int btnCamera = 2131296402;
+			
 			// aapt resource value: 0x7f090065
 			public const int buttonPanel = 2131296357;
 			
-			// aapt resource value: 0x7f0900a3
-			public const int cancel_action = 2131296419;
+			// aapt resource value: 0x7f0900a5
+			public const int cancel_action = 2131296421;
 			
 			// aapt resource value: 0x7f090047
 			public const int center = 2131296327;
@@ -6608,8 +6611,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090075
 			public const int checkbox = 2131296373;
 			
-			// aapt resource value: 0x7f0900ab
-			public const int chronometer = 2131296427;
+			// aapt resource value: 0x7f0900ad
+			public const int chronometer = 2131296429;
 			
 			// aapt resource value: 0x7f090050
 			public const int clip_horizontal = 2131296336;
@@ -6620,14 +6623,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09003b
 			public const int collapseActionView = 2131296315;
 			
-			// aapt resource value: 0x7f090093
-			public const int container = 2131296403;
+			// aapt resource value: 0x7f090095
+			public const int container = 2131296405;
 			
 			// aapt resource value: 0x7f090068
 			public const int contentPanel = 2131296360;
 			
-			// aapt resource value: 0x7f090094
-			public const int coordinator = 2131296404;
+			// aapt resource value: 0x7f090096
+			public const int coordinator = 2131296406;
 			
 			// aapt resource value: 0x7f09006f
 			public const int custom = 2131296367;
@@ -6641,20 +6644,20 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090062
 			public const int default_activity_button = 2131296354;
 			
-			// aapt resource value: 0x7f090096
-			public const int design_bottom_sheet = 2131296406;
+			// aapt resource value: 0x7f090098
+			public const int design_bottom_sheet = 2131296408;
+			
+			// aapt resource value: 0x7f09009f
+			public const int design_menu_item_action_area = 2131296415;
+			
+			// aapt resource value: 0x7f09009e
+			public const int design_menu_item_action_area_stub = 2131296414;
 			
 			// aapt resource value: 0x7f09009d
-			public const int design_menu_item_action_area = 2131296413;
+			public const int design_menu_item_text = 2131296413;
 			
 			// aapt resource value: 0x7f09009c
-			public const int design_menu_item_action_area_stub = 2131296412;
-			
-			// aapt resource value: 0x7f09009b
-			public const int design_menu_item_text = 2131296411;
-			
-			// aapt resource value: 0x7f09009a
-			public const int design_navigation_view = 2131296410;
+			public const int design_navigation_view = 2131296412;
 			
 			// aapt resource value: 0x7f090022
 			public const int disableHome = 2131296290;
@@ -6665,8 +6668,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090032
 			public const int end = 2131296306;
 			
-			// aapt resource value: 0x7f0900b1
-			public const int end_padder = 2131296433;
+			// aapt resource value: 0x7f0900b3
+			public const int end_padder = 2131296435;
 			
 			// aapt resource value: 0x7f090041
 			public const int enterAlways = 2131296321;
@@ -6710,8 +6713,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090064
 			public const int icon = 2131296356;
 			
-			// aapt resource value: 0x7f0900b0
-			public const int icon_group = 2131296432;
+			// aapt resource value: 0x7f0900b2
+			public const int icon_group = 2131296434;
 			
 			// aapt resource value: 0x7f09003c
 			public const int ifRoom = 2131296316;
@@ -6719,8 +6722,11 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090061
 			public const int image = 2131296353;
 			
-			// aapt resource value: 0x7f0900ac
-			public const int info = 2131296428;
+			// aapt resource value: 0x7f090091
+			public const int imageView = 2131296401;
+			
+			// aapt resource value: 0x7f0900ae
+			public const int info = 2131296430;
 			
 			// aapt resource value: 0x7f09005b
 			public const int italic = 2131296347;
@@ -6728,8 +6734,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090000
 			public const int item_touch_helper_previous_elevation = 2131296256;
 			
-			// aapt resource value: 0x7f090092
-			public const int largeLabel = 2131296402;
+			// aapt resource value: 0x7f090094
+			public const int largeLabel = 2131296404;
 			
 			// aapt resource value: 0x7f09004b
 			public const int left = 2131296331;
@@ -6746,14 +6752,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090063
 			public const int list_item = 2131296355;
 			
+			// aapt resource value: 0x7f0900b6
+			public const int masked = 2131296438;
+			
+			// aapt resource value: 0x7f0900a7
+			public const int media_actions = 2131296423;
+			
 			// aapt resource value: 0x7f0900b4
-			public const int masked = 2131296436;
-			
-			// aapt resource value: 0x7f0900a5
-			public const int media_actions = 2131296421;
-			
-			// aapt resource value: 0x7f0900b2
-			public const int message = 2131296434;
+			public const int message = 2131296436;
 			
 			// aapt resource value: 0x7f090033
 			public const int middle = 2131296307;
@@ -6764,8 +6770,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09002a
 			public const int multiply = 2131296298;
 			
-			// aapt resource value: 0x7f090099
-			public const int navigation_header_container = 2131296409;
+			// aapt resource value: 0x7f09009b
+			public const int navigation_header_container = 2131296411;
 			
 			// aapt resource value: 0x7f09003d
 			public const int never = 2131296317;
@@ -6776,14 +6782,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090020
 			public const int normal = 2131296288;
 			
-			// aapt resource value: 0x7f0900ae
-			public const int notification_background = 2131296430;
+			// aapt resource value: 0x7f0900b0
+			public const int notification_background = 2131296432;
 			
-			// aapt resource value: 0x7f0900a8
-			public const int notification_main_column = 2131296424;
+			// aapt resource value: 0x7f0900aa
+			public const int notification_main_column = 2131296426;
 			
-			// aapt resource value: 0x7f0900a7
-			public const int notification_main_column_container = 2131296423;
+			// aapt resource value: 0x7f0900a9
+			public const int notification_main_column_container = 2131296425;
 			
 			// aapt resource value: 0x7f09004e
 			public const int parallax = 2131296334;
@@ -6812,11 +6818,11 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09004c
 			public const int right = 2131296332;
 			
-			// aapt resource value: 0x7f0900ad
-			public const int right_icon = 2131296429;
+			// aapt resource value: 0x7f0900af
+			public const int right_icon = 2131296431;
 			
-			// aapt resource value: 0x7f0900a9
-			public const int right_side = 2131296425;
+			// aapt resource value: 0x7f0900ab
+			public const int right_side = 2131296427;
 			
 			// aapt resource value: 0x7f09000c
 			public const int save_image_matrix = 2131296268;
@@ -6890,14 +6896,14 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090027
 			public const int showTitle = 2131296295;
 			
-			// aapt resource value: 0x7f090091
-			public const int smallLabel = 2131296401;
+			// aapt resource value: 0x7f090093
+			public const int smallLabel = 2131296403;
 			
-			// aapt resource value: 0x7f090098
-			public const int snackbar_action = 2131296408;
+			// aapt resource value: 0x7f09009a
+			public const int snackbar_action = 2131296410;
 			
-			// aapt resource value: 0x7f090097
-			public const int snackbar_text = 2131296407;
+			// aapt resource value: 0x7f090099
+			public const int snackbar_text = 2131296409;
 			
 			// aapt resource value: 0x7f090045
 			public const int snap = 2131296325;
@@ -6920,8 +6926,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09004d
 			public const int start = 2131296333;
 			
-			// aapt resource value: 0x7f0900a4
-			public const int status_bar_latest_event_content = 2131296420;
+			// aapt resource value: 0x7f0900a6
+			public const int status_bar_latest_event_content = 2131296422;
 			
 			// aapt resource value: 0x7f090078
 			public const int submenuarrow = 2131296376;
@@ -6947,8 +6953,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f09006b
 			public const int textSpacerNoTitle = 2131296363;
 			
-			// aapt resource value: 0x7f09009e
-			public const int text_input_password_toggle = 2131296414;
+			// aapt resource value: 0x7f0900a0
+			public const int text_input_password_toggle = 2131296416;
 			
 			// aapt resource value: 0x7f090014
 			public const int textinput_counter = 2131296276;
@@ -6956,8 +6962,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090015
 			public const int textinput_error = 2131296277;
 			
-			// aapt resource value: 0x7f0900aa
-			public const int time = 2131296426;
+			// aapt resource value: 0x7f0900ac
+			public const int time = 2131296428;
 			
 			// aapt resource value: 0x7f09001c
 			public const int title = 2131296284;
@@ -6980,8 +6986,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090070
 			public const int topPanel = 2131296368;
 			
-			// aapt resource value: 0x7f090095
-			public const int touch_outside = 2131296405;
+			// aapt resource value: 0x7f090097
+			public const int touch_outside = 2131296407;
 			
 			// aapt resource value: 0x7f09000f
 			public const int transition_current_scene = 2131296271;
@@ -7010,8 +7016,8 @@ namespace Countr.Droid
 			// aapt resource value: 0x7f090016
 			public const int view_offset_helper = 2131296278;
 			
-			// aapt resource value: 0x7f0900b3
-			public const int visible = 2131296435;
+			// aapt resource value: 0x7f0900b5
+			public const int visible = 2131296437;
 			
 			// aapt resource value: 0x7f09003e
 			public const int withText = 2131296318;
--- Countr.Droid/Views/CountersView.cs
+++ Countr.Droid/Views/CountersView.cs
@@ -1,7 +1,12 @@
 ﻿using Android.App;
+using Android.Content;
+using Android.Graphics;
 using Android.OS;
+using Android.Provider;
+using Android.Runtime;
 using Android.Support.V7.Widget;
 using Android.Support.V7.Widget.Helper;
+using Android.Widget;
 using Countr.Core.ViewModels;
 using MvvmCross.Droid.Support.V7.AppCompat;
 using MvvmCross.Droid.Support.V7.RecyclerView;
@@ -11,13 +16,15 @@ namespace Countr.Droid.Views
     [Activity(Label = "@string/ApplicationName")]
     public class CountersView : MvxAppCompatActivity<CountersViewModel>
     {
+        private ImageView imageView;
+
         protected override void OnCreate(Bundle bundle)
         {
             base.OnCreate(bundle);
 
             SetContentView(Resource.Layout.counters_view);
 
-            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
             var recyclerView = FindViewById<MvxRecyclerView>(Resource.Id.recycler_view);
@@ -26,6 +33,22 @@ namespace Countr.Droid.Views
             var callback = new SwipeItemTouchHelperCallback(ViewModel);
             var touchHelper = new ItemTouchHelper(callback);
             touchHelper.AttachToRecyclerView(recyclerView);
+
+            //Start camera code:
+            var btnCamera = FindViewById<Button>(Resource.Id.btnCamera);
+            imageView = FindViewById<ImageView>(Resource.Id.imageView);
+            btnCamera.Click += BtnCamera_Click;
+        }
+        protected override void OnActivityResult(int requestCode, [GeneratedEnum] Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            Bitmap bitmap = (Bitmap)data.Extras.Get("data");
+            imageView.SetImageBitmap(bitmap);
+        }
+        private void BtnCamera_Click(object sender, System.EventArgs e)
+        {
+            Intent intent = new Intent(MediaStore.ActionImageCapture);
+            StartActivityForResult(intent, 0);
         }
     }
 }
